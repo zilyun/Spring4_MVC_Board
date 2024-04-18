e5947e76ecf0ff29a674fccfb72feead41be3a1f
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-04-18 12:11:20 +0900

    Study: Spring MVC Board 교육 - 1

---
+++ src/main/java/com/naver/myhome4/Spring4MvcBoardApplication.java
@@ -0,0 +1,13 @@
+package com.naver.myhome4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4MvcBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4MvcBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naver/myhome4/Spring4MvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.myhome4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4MvcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
